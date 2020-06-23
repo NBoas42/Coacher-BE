8aa306de268b836e2e6b24fdf1cd01779ccf1cd4
Nate Boas <Macbook@ip-10-0-0-247.us-west-2.compute.internal>
2020-06-22 17:57:00 -0700

    feature/user Added ENV variables to database connection

--- src/modules/user/dto/createUser.dto.ts
+++ src/modules/user/dto/createUser.dto.ts
@@ -1,23 +1,32 @@
 import { USER_TYPE } from "../model/user.interface";
-import { IsEnum, IsString, IsDefined } from "class-validator";
+import { IsEnum, IsString, IsDefined, IsOptional } from "class-validator";
 
 export class CreateUserDto {
    @IsEnum(USER_TYPE)
    @IsDefined()
    type: USER_TYPE;
+
    @IsString()
    @IsDefined()
    firstName: string;
+
    @IsString()
    @IsDefined()
    lastName:string;
+
    @IsString()
    @IsDefined()
    email: string;
+
    @IsString()
+   @IsOptional()
    phoneNumber:string;
+
    @IsString()
+   @IsOptional()
    website:string;
+   
    @IsString()
+   @IsOptional()
    about:string;
   }
